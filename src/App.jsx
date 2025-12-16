ca9dd00ca6a25c38772244e4baab4ec0003f4780
jananikodithuwakku <163711589+jananikodithuwakku@users.noreply.github.com>
2025-12-16 11:56:33 +0530

    Created Assignment 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
       <ul>
          <h2 className="font_style">Navigation Menu</h2>
 
-        <li><a href="#/assignment_1">Device Orientation</a></li>
+        <li><a href="#/assignment_1">UI Design 01</a></li>
+        <li><a href="#/assignment_2">UI Design 02</a></li>
       </ul>
     </>
   );
---
+++ src/assignment/Assignment_2.jsx
@@ -0,0 +1,20 @@
+import "./Assignment_2.css";
+
+export default function Assignment_2() {
+    return(
+        <div className="container">
+            <div className="box header2">HEADER</div>
+
+            <div className="features">
+                <div className="box feature feature1">FEATURE</div>
+                <div className="box feature feature2">FEATURE</div>
+                <div className="box feature feature3">FEATURE</div>
+            </div>
+
+            <div className="box signup">SIGN UP</div>
+
+            <div className="box content2">CONTENT</div>
+            
+        </div>
+    );
+}
